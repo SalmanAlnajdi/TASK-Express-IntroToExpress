49fbb53316f72853b02e4d81968648bad6301937
Salman Alnajdi <salman.it095@gmail.com>
2024-06-05 20:01:09 +0300

    update delete part

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 const express = require('express');
+const products = require('./data.js');
 
 const app = express();
 const PORT = 8000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.use(express.json());
+
+app.get('/products', (req, res) => {
+   return   res.json(products);
+});
+
+
+app.get('/products/:id', (req, res) => {
+        const product = products.find(p => p.id == req.params.id);
+        return res.json(product);
+    });
+
+app.post('/products', (req, res) => {
+        products.push(req.body);
+        return res.json(products);
+    });
+ 
+
+
+app.delete('/products/:id', (req, res) => {
+    const product = products.find(p => p.id == req.params.id);
+    products.splice(products.indexOf(product), 1);
+    return res.json(products);
 });
 
 app.listen(PORT, () => {
